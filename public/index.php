0c651521f0dea77cea4c7e4dc274535b50757b65
Vladislav Soprun <develop@soprun.com>
2020-07-09 04:18:28 +0300

    commit...

--- public/index.php
+++ public/index.php
@@ -15,12 +15,17 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-if (($trustedProxies = $_SERVER['TRUSTED_PROXIES']) ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+if (empty($_SERVER['TRUSTED_PROXIES']) === false) {
+    Request::setTrustedProxies(
+        explode(',', $_SERVER['TRUSTED_PROXIES']),
+        Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST
+    );
 }
 
-if (($trustedHosts = $_SERVER['TRUSTED_HOSTS']) ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
+if (empty($_SERVER['TRUSTED_HOSTS']) === false) {
+    Request::setTrustedHosts([
+        $_SERVER['TRUSTED_HOSTS']
+    ]);
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool)$_SERVER['APP_DEBUG']);
